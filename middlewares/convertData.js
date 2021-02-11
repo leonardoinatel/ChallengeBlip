778c4336f9c785364ac62f3288c4af7e35e2dd18
leonardoinatel <leonardodamasceno@gec.inatel.br>
2021-02-11 12:54:22 -0300

    fixed: fixed json to send to caroucel

--- middlewares/convertData.js
+++ middlewares/convertData.js
@@ -16,14 +16,13 @@ const convertApi = async (req, res, next) => {
   const array = arraySort(csharpOrderByDate, 'created_at').slice(0,5);
   const cSharpSorted = arraySort(array, 'created_at', { reverse: true })
   const result = cSharpSorted.map(({full_name, owner: { avatar_url }, description }, index) => (
-    { header: { 
-        type: "application/vnd.lime.media-link+json",
-        value: { 
-          title: full_name,
-          text: description,
-          type: "image/png",
-          uri: avatar_url,
-        }
+    {
+      type: "application/vnd.lime.media-link+json",
+      value: { 
+        title: full_name,
+        text: description,
+        type: "image/png",
+        uri: avatar_url,
       },
       options: [
         {
